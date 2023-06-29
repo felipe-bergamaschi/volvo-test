caa416c78a873b2687c08e9386d82c2a2388bc57
Felipe Bergamaschi <midisoft.corporation@gmail.com>
2023-06-29 14:29:12 -0300

    feat: add list cars

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,38 @@
 'use client'
 
+import { Body } from '@/components/body'
 import { Header } from '@/components/header'
 import { Hero } from '@/components/hero'
-import { Button, Box } from '@mui/material'
+import { useCars } from '@/query/useCars'
+import { Box } from '@mui/material'
 
 export default function Home() {
+  const { isLoading, error, data } = useCars()
+
+  console.log({ isLoading, error, data })
+
+  if (error) {
+    return (
+      <div>
+        deu erro
+      </div>
+    )
+  }
+
   return (
     <Box>
       <Header />
 
       <Hero />
 
-      <Button variant="contained" color="primary">
-        Hello World
-      </Button>
+      {/* TODO: faz um loading descente por favor */}
+      {isLoading ? (
+        <div>
+          loading
+        </div>
+      ) : (
+        <Body data={data} />
+      )}
     </Box>
   )
 }
