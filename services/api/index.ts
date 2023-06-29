3235c1ff14c273d6cb278f3e70e20ff6fe6ab529
Felipe Bergamaschi <midisoft.corporation@gmail.com>
2023-06-29 18:03:42 -0300

    fix: base url

--- services/api/index.ts
+++ services/api/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = process.env.NEXT_PUBLIC_VERCEL_URL || 'http://localhost:3000'
+const baseURL = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'
 
 console.log('baseURL', baseURL)
 
