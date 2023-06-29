b6f51046cc8c2e121a895313b93132d18eb3447f
Felipe Bergamaschi <midisoft.corporation@gmail.com>
2023-06-29 17:10:31 -0300

    feat: add slick

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 'use client'
 
 import "./globals.css";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import { createTheme } from '@mui/material/styles';
