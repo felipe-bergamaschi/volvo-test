04019eae2c4b9afd16ef3b45a396999dc7761550
Felipe Bergamaschi <midisoft.corporation@gmail.com>
2023-06-29 13:10:21 -0300

    feat: add get cars api

---
+++ app/api/useCars/route.ts
@@ -0,0 +1,33 @@
+import fs from "fs";
+import path from 'path';
+import util from 'util';
+import { NextResponse } from "next/server";
+
+const readFileAsync = util.promisify(fs.readFile);
+
+export async function GET() {
+  const filePath = path.join(process.cwd(), 'public', 'api', 'cars.json');
+
+  try {
+    const data = await readFileAsync(filePath, 'utf8');
+    const jsonData = JSON.parse(data);
+
+    const response = {
+      status: 200,
+      body: {
+        data: jsonData
+      }
+    };
+
+    return NextResponse.json(response);
+  } catch (error) {
+    const response = {
+      status: 200,
+      body: {
+        data: null
+      }
+    };
+
+    return NextResponse.json(response);
+  }
+}
