ec2eb11b9b6df5d778bb0154eeb85430e7e00ce6
Felipe Bergamaschi <midisoft.corporation@gmail.com>
2023-06-29 17:29:18 -0300

    fix: error

--- app/page.tsx
+++ app/page.tsx
@@ -2,25 +2,24 @@
 
 import { Hero } from '@/components/hero'
 import { CommonLayout } from '@/components/layout/common'
+import { Text } from '@/components/text'
 import { Body } from '@/features/home/body'
 import { useCars } from '@/query/useCars'
 
 export default function Home() {
   const { isLoading, error, data } = useCars()
 
-  if (error) {
-    return (
-      <div>
-        deu erro
-      </div>
-    )
-  }
-
   return (
     <CommonLayout>
       <Hero />
 
-      <Body data={data} isLoading={isLoading} />
+      {!error ? (
+        <Body data={data} isLoading={isLoading} />
+      ) : (
+        <Text variant='body'>
+          Um erro ocorreu ao carregar os carros, tente recarregar a página
+        </Text>
+      )}
     </CommonLayout>
   )
 }
